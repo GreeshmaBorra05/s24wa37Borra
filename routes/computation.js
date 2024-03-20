0faae8aa7bbe4f36198417c2c62ba4d5eba9e919
GreeshmaBorraa <156365934+GreeshmaBorraa@users.noreply.github.com>
2024-03-20 16:00:50 -0500

    updated bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  // Generate a random value for x
-  let x = Math.random() * 100;
-  
-  // Calculate the cosine of x
-  let y = Math.cos(x);
- 
-  res.send(`cos of ${x} is ${y}`);
+
+  let x = req.query.x;
+
+  if (!x || isNaN(parseFloat(x))) {
+    x = Math.random() * 100;
+  } else {
+    x = parseFloat(x);
+  }
+
+  let num1  = Math.random();
+  let cosres = Math.cos(x);
+  let sinh = Math.asinh(x);
+  num1 = Math.min(Math.max(num1, -1), 1);
+  let sinvalue = Math.asin(num1);
+  res.send(`Math.cos applied to ${x} is ${cosres} <br>
+  Math.asin applied to ${num1} is ${sinvalue} <br>
+  Math.asinh applied to ${x} is ${sinh} <br>  
+  `);
 });
- 
+
 module.exports = router;
