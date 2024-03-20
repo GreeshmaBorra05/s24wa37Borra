c7e1b6871fe91967a7e56d23812b5bbf598d016a
GreeshmaBorraa <156365934+GreeshmaBorraa@users.noreply.github.com>
2024-03-20 15:28:02 -0500

    Update computation.js

--- views/computation.js
+++ views/computation.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  // Generate a random value for x
-  let x = Math.random() * 100;
-  
-  // Calculate the cosine of x
+
+  let x = req.query.x;
+
+  if (!x || isNaN(parseFloat(x))) {
+    x = Math.random() * 100;
+  } else {
+    x = parseFloat(x);
+  }
+
   let y = Math.cos(x);
  
   res.send(`cos of ${x} is ${y}`);
 });
- 
+
 module.exports = router;
