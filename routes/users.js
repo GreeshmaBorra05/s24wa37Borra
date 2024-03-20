6949d8e30fee27462b119161e148b385478a858f
GreeshmaBorraa <156365934+GreeshmaBorraa@users.noreply.github.com>
2024-03-20 13:25:23 -0500

    h

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+var express = require('express');
+var router = express.Router();
+var product = 1 ;
+var factor = 1;
+ 
+ 
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  factor++;
+  product = product*factor;
+  res.send(`The Product with factor is: ${product}`);
+});
+ 
+module.exports = router;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
